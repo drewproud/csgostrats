bd610ea3666d5f434dd5def7d6f1df6806e1221d
Duane Pugh <duane.a.pugh@gmail.com>
2016-01-10 10:05:11 -0800

    test

--- meteor/lib/strats.js
+++ meteor/lib/strats.js
@@ -1,11 +1,58 @@
 Strats = [
   {
+    mapName: 'Inferno',
+    strats: [
+      {
+        stratName: 'Rash B No Stop',
+        side: 't',
+        url: 'img/DE_INFERNO.png'
+      },
+      {
+        stratName: 'Hold B No Stop',
+        side: 'ct',
+        url: 'img/dust2ct.jpeg'
+      },
+      {
+        stratName: 'Hold B No Stop',
+        side: 'ct',
+        url: 'img/dust2ct.jpeg'
+      },
+    ]
+  },
+//BREAK
+  {
+    mapName: 'Cobblestone',
+    strats: [
+      {
+        stratName: 'Rash B No Stop',
+        side: 't',
+        url: 'img/dust2test.jpeg'
+      },
+      {
+        stratName: 'Hold B No Stop',
+        side: 'ct',
+        url: 'img/dust2ct.jpeg'
+      },
+      {
+        stratName: 'Hold B No Stop',
+        side: 'ct',
+        url: 'img/dust2ct.jpeg'
+      },
+    ]
+  },
+//BREAK
+  {
     mapName: 'Dust 2',
     strats: [
       {
         stratName: 'Rash B No Stop',
         side: 't',
         url: 'img/dust2test.jpeg'
+      },
+      {
+        stratName: 'Hold B No Stop',
+        side: 'ct',
+        url: 'img/dust2ct.jpeg'
       },
       {
         stratName: 'Hold B No Stop',
